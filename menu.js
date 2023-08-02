803640e5856fbdfdd27160d3451c05c113d2f36d
Federico Gomez Conde <fedeic8@gmail.com>
2023-08-02 16:24:42 -0300

    se agrega registro de transferencias

--- menu.js
+++ menu.js
@@ -1,4 +1,6 @@
-let transactionNumber = 1;
+let transactionNumber = 200;
+let registeredAccounts = 100;
+const users = [];
 
 function User(name, lastname, email, password = '1234') {
   this.name = name;
@@ -8,27 +10,12 @@ function User(name, lastname, email, password = '1234') {
   this.accounts = [];
 }
 
-User.prototype.addAccount = function (account) {this.accounts.push(account)};
-
-
-function Account(accountNumber, currency, initialBalance){
+function Account(accountNumber, currency, typeAccount, initialBalance){
   this.accountNumber = accountNumber;
   this.currency = currency;
+  this.typeAccount = typeAccount;  // Caja de Ahorro (CA) | Cuenta Corriente (CC)
   this.balance = initialBalance;
   this.transfers = [];
-}
-
-Account.prototype.transfer = function (receiverAccount, amount) {
-  if (this.currency === receiverAccount.currency && amount <= this.balance) {
-        this.balance -= amount;
-        receiverAccount.balance += amount;
-        transactionNumber++;
-        const transferNumber = transactionNumber.toString().padStart(5, "0");
-        this.transfers.push(new Transfer(amount, this.currency, this.accountNumber, receiverAccount.accountNumber, transferNumber));
-        return true;
-      } else {
-        return false; // Fondos insuficientes
-      }
 }
 
 function Transfer(amount, currency, senderAccount, receiverAccount, transferNumber) {
@@ -40,26 +27,44 @@ function Transfer(amount, currency, senderAccount, receiverAccount, transferNumb
   this.date = new Date().toLocaleString();  // para uso práctico (local). No es la forma correcta de implementarlo.
 }
 
-const users = [];
+User.prototype.addAccount = function (account) {this.accounts.push(account)};
+
+Account.prototype.newTransfer = function (receiverAccount, amount) {
+  if (this.currency === receiverAccount.currency && amount <= this.balance) {
+        this.balance -= amount;
+        receiverAccount.balance += amount;
+        // register information for each account
+        transactionNumber++;
+        const transferNumber = transactionNumber.toString().padStart(7, "0");  // '0000201'
+        const transferInformation = new Transfer(amount, this.currency, this.accountNumber, receiverAccount.accountNumber, transferNumber);
+        this.transfers.push(transferInformation);
+        receiverAccount.transfers.push(transferInformation);
+        return true;
+      } else {
+        return false; // Fondos insuficientes o el tipo de moneda es distinto
+      }
+}
+
 
 function createUser(name, lastname, email, password = '1234') {
   const newUser = new User(name, lastname, email, password);
   users.push(newUser);
-  createAccount(newUser);
+  createAccount(newUser, 'ARS', 'CA', 1000);  // cuando se crea un usuario automaticamente se le genera una CA con 1000 pesos
   
   return newUser;
 }
 
-function createAccount(user, currency = 'ARS', initialBalance) {
-  const accountNumber = `${Math.floor(Math.random() * 100_000_000)}`;
-  const arsAccount = new Account(accountNumber, currency, initialBalance);
-  user.addAccount(arsAccount);
+function createAccount(user, currency, typeAccount, initialBalance) {
+  registeredAccounts++;
+  const accountNumber = `${typeAccount}` + registeredAccounts.toString();
+  const newAccount = new Account(accountNumber, currency, typeAccount, initialBalance);
+  user.addAccount(newAccount);
 
   return user.accounts;
 }
 
-/* createUser('John', 'Doe', 'john@example.com'),
-createUser('Jane', 'Smith', 'jane@example.com'),
+let user1 = createUser('John', 'Doe', 'john@example.com');
+let user2 = createUser('Jane', 'Smith', 'jane@example.com');
 createUser('Michael', 'Johnson', 'michael@example.com'),
 createUser('Emily', 'Brown', 'emily@example.com'),
 createUser('William', 'Jones', 'william@example.com'),
@@ -67,11 +72,22 @@ createUser('Olivia', 'Garcia', 'olivia@example.com'),
 createUser('James', 'Martinez', 'james@example.com'),
 createUser('Sophia', 'Lee', 'sophia@example.com'),
 createUser('Alexander', 'Rodriguez', 'alexander@example.com'),
-createUser('Abigail', 'Lopez', 'abigail@example.com') */
+createUser('Abigail', 'Lopez', 'abigail@example.com')
+
+// Simulando una transferencia //
+// antes
+console.log(user1.accounts);
+console.log(user2.accounts);
+//transferencia
+user1.accounts[0].newTransfer(user2.accounts[0], 500);
+//despues
+console.log(user1.accounts);
+console.log(user2.accounts);
+console.log(user1.accounts[0].transfers[0]);
+console.log(user2.accounts[0].transfers[0]);
 
 
-// chequear que las cuentas emisor y receptor sean de la misma moneda
-// usar prototype para asignar funciones a los usuarios -> user.prototype.transfer = function..
+
 
 // function buscarUsuario(callback){
 //   const usuario = prompt(`Ingrese el ID o Email del usuario a buscar`);
